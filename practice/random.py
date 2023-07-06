e3b7e4485e51bd33797238bc6ad66b9dd1a3a9fb
Bhabaranjan Panigrahi <112833588+bhaba-ranjan@users.noreply.github.com>
2023-07-06 13:36:40 -0400

    fix: config gile and practice files added

---
+++ practice/random.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+import pandas as pd
+import PIL as pl
+import scipy as sp
+import sys
+
+print(f'tensorflow == {tf.__version__}')
+print(f'tensorflow-datasets == {tfds.__version__}')
+print(f'numpy == {np.__version__}')
+print(f'pandas == {pd.__version__}')
+print(f' PIL == {pl.__version__}')
+print(f'scipy == {sp.__version__}')
+print(f'python == {sys.version}')
